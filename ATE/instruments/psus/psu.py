8b13e936fc9837e63c0570742f631bd364a26845
Peter Phelan <pphelan@sciaps.com>
2023-03-30 10:13:24 -0400

    power supply logging script

--- ATE/instruments/psus/psu.py
+++ ATE/instruments/psus/psu.py
@@ -2,7 +2,8 @@ import pyvisa
 
 from ATE.instruments.instrument import Instrument
 
-from time import time, sleep
+from time import time, gmtime, sleep, strftime
+import csv
 
 class Psu(Instrument):
     ## class vars ##
@@ -61,6 +62,12 @@ class Psu(Instrument):
     def output_disable(self, ch_val: int) -> None:
         self._wr(f'OUTPUT CH{ch_val},OFF')
 
+    def show_wave(self, ch_val: int) -> None:
+        self._wr(f'OUTPUT:WAVE CH{ch_val},ON')
+
+    def hide_wave(self, ch_val: int) -> None:
+        self._wr(f'OUTPUT:WAVE CH{ch_val},ON')
+
     # measurements
 
     # @property
@@ -76,13 +83,14 @@ if __name__ == "__main__":
     print(f'Device ID: {psu.id}')
 
     # set voltage and current limits
-    psu.voltage_limit_write(1, 5)
+    psu.voltage_limit_write(1, 24)
     print(f'Channel 1 Voltage Limit = {psu.voltage_limit_read(1)}')
 
-    psu.current_limit_write(1, 1.5)
+    psu.current_limit_write(1, 3)
     print(f'Channel 1 Current Limit = {psu.current_limit_read(1)}')
 
     # enable outputs
+    psu.show_wave(1)
     psu.output_enable(1)
     print('Enabling output and starting log.  Press Ctrl+C to stop.')
     # sleep(0.1)
@@ -90,16 +98,22 @@ if __name__ == "__main__":
 
     # datalog loop
     # for i in range(10):
-    while True:
-        try:
-            t_samp = time() - t0
-            v_meas = psu.measure_voltage(1)
-            i_meas = psu.measure_current(1)
-            print(f't={t_samp:.3f}: v={v_meas} i={i_meas}')
-            sleep(0.1)
-        except KeyboardInterrupt:
-            print('stopping log')
-            break
+    with open(f'SPD3303X_log_{strftime("%m-%d-%y_%H-%M-%S", gmtime(t0))}.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow([f'Test start time {strftime("%m-%d-%y_%H:%M:%S", gmtime(t0))}'])
+        writer.writerow(['Time(s)', 'Voltage (V)', 'Current (A)', 'Power (W)'])
+
+        while True:
+            try:
+                t_samp = time() - t0
+                v_meas = psu.measure_voltage(1)
+                i_meas = psu.measure_current(1)
+                p_meas = psu.measure_power(1)
+                writer.writerow(f'{t_samp:.3f},{v_meas},{i_meas},{p_meas}'.split(','))
+                sleep(0.1)
+            except KeyboardInterrupt:
+                print('stopping log')
+                break
 
     print('disabling output')
     psu.close()
